e1b5cb04aea7e30b55bf3136ae6a3c07cc93d112
jialok0218 <jialok0218@gmail.com>
2024-12-02 22:31:34 +0800

    modify gallery grid

--- src/app/gallery/sections/GalleryGrid.tsx
+++ src/app/gallery/sections/GalleryGrid.tsx
@@ -47,7 +47,7 @@ export const GalleryGrid: React.FC<GalleryGridProps> = ({ images }) => {
           </p>
         </motion.div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {images.map((image, index) => (
             <motion.div
               key={index}
